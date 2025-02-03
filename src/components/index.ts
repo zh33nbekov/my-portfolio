c9584878cd19d0233dc01b7ec920c92c4be7683c
zh33nbekov <rai.zheenbekov@gmail.com>
2025-02-03 22:29:45 +0600

    added drawer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './button/Button'
+export * from './drawer/Drawer'
 export * from './header/Header'
 export * from './image-card/ImageCard'
 export * from './section-title/SectionTitle'
