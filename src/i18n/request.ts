b2d0b587f831235105edfa1dac21b0290bb2304a
zh33nbekov <rai.zheenbekov@gmail.com>
2025-02-06 17:23:06 +0600

    dev/fix

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,7 +3,7 @@ import { routing } from './routing'
 
 export default getRequestConfig(async ({ requestLocale }) => {
 	let locale = await requestLocale
-	if (!locale || !routing.locales.includes(locale as any)) {
+	if (!locale || !routing.locales.includes(locale as 'ru' | 'en')) {
 		locale = routing.defaultLocale
 	}
 
