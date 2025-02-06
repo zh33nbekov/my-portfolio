1a7b41c305be29c9e32367d9927bce65701d2515
zh33nbekov <rai.zheenbekov@gmail.com>
2025-02-06 16:47:00 +0600

    dev/fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,7 @@
 const NotFound = () => (
-	<html>
-		<body>
-			<div style={{ textAlign: 'center', marginTop: '50px' }}>
-				<h1>404 - Page Not Found</h1>
-				<p>The page you are looking for does not exist.</p>
-			</div>
-		</body>
-	</html>
+	<div style={{ textAlign: 'center', marginTop: '50px' }}>
+		<h1>404 - Page Not Found</h1>
+		<p>The page you are looking for does not exist.</p>
+	</div>
 )
 export default NotFound
