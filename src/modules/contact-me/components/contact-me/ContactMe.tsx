e4a1ae70b51c1dd855b346d8d860082b2104c432
zh33nbekov <rai.zheenbekov@gmail.com>
2025-02-08 04:51:47 +0600

    contact-me/fixed

--- src/modules/contact-me/components/contact-me/ContactMe.tsx
+++ src/modules/contact-me/components/contact-me/ContactMe.tsx
@@ -8,7 +8,7 @@ import { FeedbackForm } from '../feedback-form/FeedbackForm'
 
 export const ContactMe = () => (
 	<section id={HEADER_LINKS_OPTIONS.CONTACT.PATH} className={styles.contactMe}>
-		<SectionTitle title='ContactMe' width='50' />
+		<SectionTitle title='ContactMe' width='100' />
 		<div className={styles.contactMe__content}>
 			<div className={styles.contactMe__image}>
 				<Image src={ContactMeImg} alt='Связаться со мной' />
